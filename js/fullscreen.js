83ab744560bb3cb16813dc4ff6aa3e4509d695ab
Thomas de Queiroz Barros <38295417+thomasqueirozb@users.noreply.github.com>
2018-11-28 09:49:39 -0200

    Add files via upload

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let html_all = document.querySelector('html')
-  let nav = document.querySelector('nav')
-
-  nav.addEventListener('click', () => {
-    if(html_all.requestFullscreen) {
-      html_all.requestFullscreen()
-    }
-    else if(html_all.msRequestFullscreen) {
-      html_all.msRequestFullscreen()
-    }
-    else if(html_all.mozRequestFullScreen) {
-      html_all.mozRequestFullScreen()
-    }
-    else if(html_all.webkitRequestFullscreen) {
-      html_all.webkitRequestFullscreen()
-    }
-  })
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//   let html_all = document.querySelector('html')
+//   let nav = document.querySelector('nav')
+//
+//   nav.addEventListener('click', () => {
+//     if(html_all.requestFullscreen) {
+//       html_all.requestFullscreen()
+//     }
+//     else if(html_all.msRequestFullscreen) {
+//       html_all.msRequestFullscreen()
+//     }
+//     else if(html_all.mozRequestFullScreen) {
+//       html_all.mozRequestFullScreen()
+//     }
+//     else if(html_all.webkitRequestFullscreen) {
+//       html_all.webkitRequestFullscreen()
+//     }
+//   })
+// })
